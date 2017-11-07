2c9cacf55efaa9d6337a1edc339469f5b53d8251
rfyiamcool <rfyiamcool@163.com>
2017-11-07 12:28:24 +0800

    pep8

--- worker.py
+++ worker.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import os
 import time
 
 from log import logger
+
 
 #你的业务逻辑
 def kworker_handler():
     time.sleep(5)
     return True
 
+
 def worker_handler():
     time.sleep(0.01)
     logger.info('this is worker_handler')
 
-ALLOW_METHOD = [{"func":kworker_handler,"counte":1},{"func":worker_handler,"count":3}]
 
+ALLOW_METHOD = [{"func": kworker_handler, "counte": 1}, {"func": worker_handler, "count": 3}]
